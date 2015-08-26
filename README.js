c8e8e00f3cb0c99e051214795d5e3e109f708f22
Kyle Mitchell <kyle@kemitchell.com>
2015-08-25 23:32:02 -0700

    Upgrade to commonform-flatten@0.6.x

---
+++ README.js
@@ -0,0 +1,11 @@
+require('tape')('README', function(test)  {
+  var md = require('./');
+  var form = {
+      content: [{
+              heading: 'A Heading',
+              form: { content: ['Some text'] }
+          }]
+  };
+  test.deepEqual(typeof md(form), 'string', 'line 9');
+  test.end();
+});
